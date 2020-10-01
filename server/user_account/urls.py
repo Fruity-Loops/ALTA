c2312a45f80de4d3837e59f8271e60a052ce492b
Robert <robertbeaudenon@gmail.com>
2020-09-30 21:03:26 -0400

    feat: Adding new routes to redirect api calls
    The new introduced routes allows system admin to register,login and logout.

--- server/user_account/urls.py
+++ server/user_account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
-from rest_framework import routers 
+from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('auth_users', views.AuthUsersView) # params: endpoint, view
+router.register(r'registration', views.RegistrationView)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', views.LoginView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
 ]
