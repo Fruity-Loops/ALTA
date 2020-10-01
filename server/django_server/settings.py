a3da6bdf124045b2bf984c846b8cf1f462600181
Robert <robertbeaudenon@gmail.com>
2020-09-30 23:18:57 -0400

    feat: hashing password before insert in db using sha256

--- server/django_server/settings.py
+++ server/django_server/settings.py
@@ -109,6 +109,17 @@ DATABASES = {
     }
 }
 
+# password hashing
+PASSWORD_HASHERS = (
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.SHA1PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher',
+    'django.contrib.auth.hashers.CryptPasswordHasher',
+)
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
