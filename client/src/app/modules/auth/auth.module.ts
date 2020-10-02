c3b07f7e6459233463ce8e996a59f4662092c625
Robert <robertbeaudenon@gmail.com>
2020-10-02 17:56:49 -0400

    fix: adding required dependancies to module

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //Reactive forms provide a model-driven approach to handling form inputs whose values change over tim
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AuthTabsComponent } from '../../components/auth-tabs/auth-tabs.component';
+import { LoginComponent } from 'src/app/components/login/login.component';
 
 @NgModule({
-  declarations: [AuthTabsComponent],
-  imports: [CommonModule],
-  exports: [AuthTabsComponent],
+  declarations: [AuthTabsComponent, LoginComponent],
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [AuthTabsComponent, LoginComponent],
 })
 export class AuthModule {}
