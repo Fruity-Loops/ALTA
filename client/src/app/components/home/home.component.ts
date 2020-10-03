9cc0c4b6e32d463395ef0462f2150a41db816cea
Robert <robertbeaudenon@gmail.com>
2020-10-03 17:48:32 -0400

    feat: deleting cookies when user logout

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TokenService } from 'src/app/services/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private tokenService: TokenService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  /****TO LOGOUT*****/
+  logout() {
+    this.tokenService.DeleteToken(); //Delete token when user logout
+    this.router.navigate(['']); //redirect user to login/register pager
+
+    //TODO: Check out if we want to delete also the token from the db, in order to regenerate a new one while logging in
   }
-
 }
--- client/src/app/modules/alta-main/alta-main.module.ts
+++ client/src/app/modules/alta-main/alta-main.module.ts
@@ -4,9 +4,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { AuthService } from 'src/app/services/auth.service';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule, RouterModule],
+  providers: [AuthService],
 })
 export class AltaMainModule {}
