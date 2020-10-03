62d1f22949c1f0174169e10b1da06cea48f3ed32
Robert <robertbeaudenon@gmail.com>
2020-10-03 01:32:56 -0400

    feat: adding guards to verify if token is still valid

--- client/src/app/modules/alta-main-routing/alta-main-routing.module.ts
+++ client/src/app/modules/alta-main-routing/alta-main-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/app/components/home/home.component';
+import { AuthGuard } from '../../guards/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+    canActivate: [AuthGuard], //if user has a valid token he will be able to access comment page
   },
 ];
 
