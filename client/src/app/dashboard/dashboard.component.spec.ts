1d30d52746807e4c99c83b1780aace8d57896b1f
Avery <averyndersingh@gmail.com>
2020-10-04 17:48:36 -0400

    test: added test functions
    These functions now test that each option is clickable within the list, that when clicking a specific option the right output is displayed and with nothing clicked the initial/ default option is the right one

--- client/src/app/dashboard/dashboard.component.spec.ts
+++ client/src/app/dashboard/dashboard.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
@@ -22,4 +22,33 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // Default Option
+  it('#onSelect should set #selectedOption to "The First option"', () => {
+    expect(component.selectedOption.title).toBe("Create My Account", 'Initial Option');
+  });
+
+  // Tests Options are clickable
+  it('should test click 2nd option', () => {
+    const theSpy = spyOn(component, 'onSelect');
+    const elements = fixture.debugElement.queryAll(By.css('li'));
+    elements.forEach(element => {
+      fixture.detectChanges();
+      element.nativeElement.click();
+      expect(component.onSelect).toHaveBeenCalled();
+      theSpy.calls.reset();
+    });
+  });
+
+  // Tests 2nd option was clicked
+  it('should test header changed to Modify My Account', () => {
+    const elements = fixture.debugElement.queryAll(By.css('li'));
+    elements[1].nativeElement.click();
+    fixture.detectChanges();
+
+    const paragraphDe = fixture.debugElement.query(By.css('h3'));
+    const header: HTMLElement = paragraphDe.nativeElement;
+    expect(header.textContent).toEqual('Current Setting: Modify My Account');
+  });
+
 });
