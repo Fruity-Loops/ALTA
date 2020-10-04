532dbc4618c9a153d0c7755c9ba7d9290be78a8c
Avery <averyndersingh@gmail.com>
2020-10-04 17:52:13 -0400

    docs: Added comments
    Added some comments to what the variables in the dashboard component do as well as where the listing is coming from

--- client/src/app/dashboardListing.ts
+++ client/src/app/dashboardListing.ts
@@ -1,5 +1,6 @@
 import { DashboardOption } from './dashboardOption';
 
+// These are the menu options listed that can be chosen from the dashboard
 export const DashboardListings: DashboardOption[] = [
   { title: 'Create My Account' },
   { title: 'Modify My Account' },
