f87b34abd1402fc753cd454b5d5a945ad01767d4
Robert <robertbeaudenon@gmail.com>
2020-10-06 19:54:48 -0400

    test: testing services

--- client/src/app/services/token.service.spec.ts
+++ client/src/app/services/token.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { TokenService } from './token.service';
 
 describe('TokenService', () => {
@@ -12,5 +11,19 @@ describe('TokenService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should set a token', () => {
+    service.SetToken('Token');
+    expect(service.GetToken()).toBe('Token');
+  });
+
+  it('should get token', () => {
+    expect(service.GetToken()).toBe('Token');
+  });
+
+  it('should delete a token', () => {
+    service.DeleteToken();
+    expect(service.GetToken()).toBe('');
   });
 });
