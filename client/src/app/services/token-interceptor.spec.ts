66463f3f5165be448f34da67f058d2484f43acea
Robert <robertbeaudenon@gmail.com>
2020-10-06 19:12:33 -0400

    test: verifying that interceptor is modifying headers of request

--- client/src/app/services/token-interceptor.spec.ts
+++ client/src/app/services/token-interceptor.spec.ts
@@ -1,8 +1,68 @@
-// import { TokenInterceptor } from './token-interceptor';
-// import { TokenService } from './token.service';
+// We want to test that an appropriate header configuration
+// Is added to each http request before sending it to server
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
-// describe('TokenInterceptor', () => {
-//   it('should create an instance', () => {
-//     expect(new TokenInterceptor(TokenService)).toBeTruthy();
-//   });
-// });
+import { TokenInterceptor } from './token-interceptor';
+import { AuthService } from './auth.service';
+import { TestBed, getTestBed } from '@angular/core/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+describe('TokenInterceptor', () => {
+  let authService: AuthService;
+  let httpMock: HttpTestingController;
+  const BASEURL = 'http://localhost:8000';
+
+  // BeforeEach function runs before every test
+  // Allowing us to register a new TestBed module that registers both the
+  // Service and the Interceptor.
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      // Import the HttpClient mocking services
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: TokenInterceptor,
+          multi: true,
+        },
+      ],
+    });
+
+    // Inject the http, test controller, and service-under-test
+    // As they will be referenced by each test.
+    // HttpTestingController helps us mocking and flushing http request
+    httpMock = TestBed.inject(HttpTestingController);
+    authService = TestBed.inject(AuthService);
+  });
+
+  afterEach(() => {
+    // We are calling verify() method after each test to make sure that theres
+    // No unmatched outstanding request
+    httpMock.verify();
+  });
+
+  it('should add content-type and Accept property in http header', () => {
+    authService.loginSysAdmin({}).subscribe((res) => {
+      expect(res).toBeTruthy();
+    });
+
+    const httpReq = httpMock.expectOne(`${BASEURL}/login/`);
+
+    // We can assert that the httpRequest’s request.headers object contains a header
+    // With the names of ‘Accept’ and ‘Content-Type’. If this is the case then our interceptor has successfully added.
+    expect(httpReq.request.headers.has('Content-Type')).toEqual(true);
+    expect(httpReq.request.headers.has('Accept')).toEqual(true);
+
+    // Now we need to check whether included ‘Accept’ and ‘Content-Type’ header contains the values we expect
+    expect(httpReq.request.headers.get('Content-Type')).toBe(
+      'application/json'
+    );
+    expect(httpReq.request.headers.get('Accept')).toBe('application/json');
+
+    //  TODO: Should check out how to see if tokens are set
+    // Expect(httpReq.request.headers.has('Authorization')).toEqual(true);
+  });
+});
