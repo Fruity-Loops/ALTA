371441f6756671c95348479c767b274a7c8ef969
makchamp <makramadaime@hotmail.com>
2020-10-07 02:12:25 -0400

    refactor:move dashboard files into dashboard component folder

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
