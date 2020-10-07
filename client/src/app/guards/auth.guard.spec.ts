681b06618ccb2a3411107665be13b9c2eb710cf4
Robert <robertbeaudenon@gmail.com>
2020-10-07 00:24:10 -0400

    test: testing guard

--- client/src/app/guards/auth.guard.spec.ts
+++ client/src/app/guards/auth.guard.spec.ts
@@ -1,16 +1,21 @@
-// import { TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { TokenService } from '../services/token.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
-// import { AuthGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
 
-// describe('AuthGuard', () => {
-//   let guard: AuthGuard;
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({});
-//     guard = TestBed.inject(AuthGuard);
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
+      providers: [{ provide: TokenService }],
+    });
+    guard = TestBed.inject(AuthGuard);
+  });
 
-//   it('should be created', () => {
-//     expect(guard).toBeTruthy();
-//   });
-// });
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
