9d51b5768978557d69c6118a0f632b549072da42
Robert <robertbeaudenon@gmail.com>
2020-10-07 00:12:18 -0400

    test: testing initialization of components that have dependancies

--- client/src/app/components/auth-tabs/auth-tabs.component.spec.ts
+++ client/src/app/components/auth-tabs/auth-tabs.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AuthTabsComponent } from './auth-tabs.component';
 
 describe('AuthTabsComponent', () => {
@@ -8,9 +7,8 @@ describe('AuthTabsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthTabsComponent ]
-    })
-    .compileComponents();
+      declarations: [AuthTabsComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -19,7 +17,7 @@ describe('AuthTabsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create auth tabs component', () => {
     expect(component).toBeTruthy();
   });
 });
--- client/src/app/components/login/login.component.spec.ts
+++ client/src/app/components/login/login.component.spec.ts
@@ -1,25 +1,43 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from 'src/app/services/auth.service';
+import { TokenService } from 'src/app/services/token.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { FormBuilder } from '@angular/forms';
+import { LoginComponent } from './login.component';
 
-// import { LoginComponent } from './login.component';
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthService;
+  let tokenService: TokenService;
 
-// describe('LoginComponent', () => {
-//   let component: LoginComponent;
-//   let fixture: ComponentFixture<LoginComponent>;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [
+        FormBuilder,
+        {
+          provide: AuthService,
+        },
+        {
+          provide: TokenService,
+        },
+      ],
+    }).compileComponents();
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ LoginComponent ]
-//     })
-//     .compileComponents();
-//   });
+    authService = TestBed.inject(AuthService);
+    tokenService = TestBed.inject(TokenService);
+  });
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(LoginComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create login component', () => {
+    expect(component).toBeTruthy();
+  });
+});
