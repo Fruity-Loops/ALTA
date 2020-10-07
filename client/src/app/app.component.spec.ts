63eb8f317315e7ae9c1380efd17fc060b0b755c0
Avery <averyndersingh@gmail.com>
2020-10-06 22:55:31 -0400

    fix: applied requested changes by Mak #7
    This removes the demo component and tests. This adds the dashboard component to the app component's test declarations removing the unnecessary warning and fixes the dashboard test titles

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,14 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
-import { DemoComponent } from './demo/demo.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         AppComponent,
-        DemoComponent,
+        DashboardComponent,
       ],
       imports: [ HttpClientTestingModule ]
     }).compileComponents();
