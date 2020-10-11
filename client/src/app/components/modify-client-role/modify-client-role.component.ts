0ce7e2150cf1628464a80412fe080f2efbb3231a
Avery <averyndersingh@gmail.com>
2020-10-11 12:55:05 -0400

    feat: Displays Users
    Now when obtain Users is clicked, the users in the database are presented with their information to view

--- client/src/app/components/modify-client-role/modify-client-role.component.ts
+++ client/src/app/components/modify-client-role/modify-client-role.component.ts
@@ -9,11 +9,13 @@ import { DashboardService } from 'src/app/services/dashboard.service'
 })
 export class ModifyClientRoleComponent implements OnInit {
 
-  display: boolean;
-  querrysett 
+  display: boolean
+  querrysett
+  users: Array<CustomUser>
 
   constructor(private DashboardService:DashboardService) {
     this.display = false;
+    this.users = new Array<CustomUser>();
   }
 
   ngOnInit(): void {
@@ -21,7 +23,36 @@ export class ModifyClientRoleComponent implements OnInit {
 
   obtainClients()
   {
-    this.DashboardService.getAllClients().subscribe(val => this.querrysett = val);
+    this.DashboardService.getAllClients().subscribe(val =>
+      {
+        this.querrysett = JSON.parse(val);
+        for (let i = 0; i < this.querrysett.length; i++)
+        {
+          this.users.push(new CustomUser(this.querrysett[i].fields));
+        }
+      });
   }
 
+}
+
+class CustomUser
+{
+  last_login: string
+  user_name: string
+  first_name: string
+  last_name: string
+  role: string
+  is_active: string
+  email: string
+
+  constructor(fields)
+  {
+    this.last_login = fields.last_login;
+    this.user_name = fields.user_name;
+    this.first_name = fields.first_name;
+    this.last_name = fields.last_name;
+    this.role = fields.role;
+    this.is_active = fields.is_active;
+    this.email = fields.email;
+  }
 }
