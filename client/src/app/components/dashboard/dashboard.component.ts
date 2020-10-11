d63311df63849a42d280e5352a9dbc9bf7906bd9
Avery <averyndersingh@gmail.com>
2020-10-11 10:56:36 -0400

    refactor: Fixed NG Lint issues
    Most of these are simple " ==> ' changes to make the report complete

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   constructor() { }
 
-  view = "Dashboard";
+  view = 'Dashboard';
 
   ngOnInit(): void {
   }
--- client/src/app/components/manage-members/manage-members.component.ts
+++ client/src/app/components/manage-members/manage-members.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ManageMembersComponent implements OnInit {
   constructor() { }
 
-  view = "Manage Members";
+  view = 'Manage Members';
 
   ngOnInit(): void {
   }
