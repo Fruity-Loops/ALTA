e76f3d68f66fe4e29a5b1821773b3723c95984d4
makchamp <makramadaime@hotmail.com>
2020-10-12 06:17:32 -0400

    test: Add test for drawer (sidenav service) toggling
    Testing if the drawer is opening/closing properly

--- client/src/app/components/home/home.component.spec.ts
+++ client/src/app/components/home/home.component.spec.ts
@@ -1,27 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { TokenService } from 'src/app/services/token.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { HomeComponent } from './home.component';
+import { SidenavService } from 'src/app/services/sidenav.service';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
   let tokenService: TokenService;
+  let sidenavService: SidenavService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeComponent],
-      imports: [HttpClientTestingModule, RouterTestingModule],
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatSidenavModule,
+        BrowserAnimationsModule],
       providers: [
         {
           provide: TokenService,
-        },
+        }, SidenavService,
       ],
     }).compileComponents();
 
     tokenService = TestBed.inject(TokenService);
+    sidenavService = TestBed.inject(SidenavService);
   });
 
   beforeEach(() => {
@@ -32,5 +42,32 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('sidenav service should toggle the drawer', () => {
+    sidenavService.setSidenav(component.sidenav);
+    // Side Nav drawer should start open
+    expect(component.sidenav.opened).toBeTrue();
+
+    // Side Nav drawer should close after toggle
+    sidenavService.toggle();
+    fixture.detectChanges();
+    expect(component.sidenav.opened).toBeFalse();
+
+    // Side Nav drawer should be open again
+    sidenavService.toggle();
+    fixture.detectChanges();
+    expect(component.sidenav.opened).toBeTrue();
+
+    // Test closing the drawer
+    sidenavService.close();
+    fixture.detectChanges();
+    expect(component.sidenav.opened).toBeFalse();
+
+    // Test opening the drawer
+    sidenavService.open();
+    fixture.detectChanges();
+    expect(component.sidenav.opened).toBeTrue();
+
   });
 });
