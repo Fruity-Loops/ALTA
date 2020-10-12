8ca7bbdd5159cb432ad66ec242a5b22f9af435c3
Robert <robertbeaudenon@gmail.com>
2020-10-12 10:35:28 -0400

    refactor: removing unused imports

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { AuthTabsComponent } from '../../components/auth-tabs/auth-tabs.component';
 import { LoginComponent } from 'src/app/components/login/login.component';
 import { SignupComponent } from 'src/app/components/signup/signup.component';
 import { AuthService } from 'src/app/services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { MatInputModule } from '@angular/material/input';
 import { MatModule } from '../material/material-module';
 @NgModule({
   declarations: [AuthTabsComponent, LoginComponent, SignupComponent],
