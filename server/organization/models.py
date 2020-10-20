ccc1eaea115e4aa887abdee1962996b7e10a37ec
Robert <robertbeaudenon@gmail.com>
2020-10-20 13:05:05 -0400

    feat: creating view to do CRUD operations on organization

--- server/organization/models.py
+++ server/organization/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Organization(models.Model):
-    ord_id = models.AutoField(primary_key=True)
+    org_id = models.AutoField(primary_key=True)
     org_name = models.CharField(max_length=256, unique=True)
     status = models.BooleanField(default=False)
