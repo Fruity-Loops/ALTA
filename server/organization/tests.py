40509c19ae72c2810954d20351829e55a97fd9fe
Robert <robertbeaudenon@gmail.com>
2020-10-21 21:54:42 -0400

    refactor: fixing issues based on PR review

--- server/organization/tests.py
+++ server/organization/tests.py
@@ -8,7 +8,7 @@ from rest_framework.test import APIClient
 from user_account.models import CustomUser
 
 
-class OpenRegistrationTestCase(APITestCase):
+class OrganizationTestCase(APITestCase):
 
     def setUp(self):
         self.client = APIClient()
