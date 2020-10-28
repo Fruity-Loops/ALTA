13f398d81dc56b5b46673e73eca3a46793dd26ec
makchamp <makramadaime@hotmail.com>
2020-10-28 00:33:29 -0400

    feat:Showing the login status for successful logins Similar to a failed login showing the error message to the user in a red box, a successful login now shows a success message in a green box

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -12,6 +12,7 @@ import { TokenService } from 'src/app/services/token.service';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   errorMessage: string;
+  successMessage: string;
   body: any;
 
   constructor(
@@ -19,7 +20,7 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private authService: AuthService,
     private tokenService: TokenService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.init();
@@ -60,10 +61,12 @@ export class LoginComponent implements OnInit {
             this.router.navigate(['']); // Redirect user to component in path:home (defined in alta-home-routing.module.ts)
           }, 1000); // Redirect the user after 1 seconds ( in case we want to add a loading bar when we click on button )
         }
+        this.successMessage = 'Login Successful';
+        this.errorMessage = null;
         this.loginForm.reset();
-        Object.keys(this.loginForm.controls).forEach(key =>{
-          this.loginForm.controls[key].setErrors(null)
-       });
+        Object.keys(this.loginForm.controls).forEach(key => {
+          this.loginForm.controls[key].setErrors(null);
+        });
       },
       (err) => {
         if (err.error.detail) {
