fceec8168b8f22503fec47e53709b071aed68718
makchamp <makramadaime@hotmail.com>
2020-10-28 01:32:43 -0400

    fix: Fix for Issue #88: Registration Access for Anyone The signin tab now does not render at all if the 'open_registration' env variable is set to false. There is not more tabs actually. Just the log in component on it's own (when the evn is set to false).

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  open_registration: false, // Set to 'true' to allow registartion from the greeting page
 };
 
 /*
