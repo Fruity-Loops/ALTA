47d1a332d804c4c4293d9a0d252f662eb5873125
makchamp <makramadaime@hotmail.com>
2020-11-03 14:44:05 -0500

    feat: Add helper spec for running global blocks In the helper file can be specified code blocks to run before/after all tests Right now is included a sanity check that no errors are emitted from the browser after each test

--- client/e2e/src/greeting.e2e-spec.ts
+++ client/e2e/src/greeting.e2e-spec.ts
@@ -12,12 +12,5 @@ describe('E2E Greeting Page', () => {
     page.navigateTo();
     expect(page.getLoginForm()).toBeTruthy();
   });
-
-  afterEach(async () => {
-    // Assert that there are no errors emitted from the browser
-    const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-    expect(logs).not.toContain(jasmine.objectContaining({
-      level: logging.Level.SEVERE,
-    } as logging.Entry));
-  });
+  
 });
