9bba60c7baa9b62b429e9384e9b31e2bb00db7a2
Robert <robertbeaudenon@gmail.com>
2020-11-19 01:17:00 -0500

    feat: created a model that holds inventory items fields

---
+++ client/src/app/models/InventoryItem.ts
@@ -0,0 +1,17 @@
+export interface InventoryItem {
+  _id: number;
+  Location: string;
+  Plant: string;
+  Zone: string;
+  Aisle: string;
+  Part_Number: string;
+  Part_Description: string;
+  Serial_Number: string;
+  Condition: string;
+  Category: string;
+  Owner: string;
+  Criticality: string;
+  Average_Cost: string;
+  Quantity: string;
+  Unit_of_Measure: string;
+}
