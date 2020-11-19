b322c9d96d253768bca51ea0a69e7be702bba476
Robert <robertbeaudenon@gmail.com>
2020-11-19 00:31:15 -0500

    feat: added service to get all items from the backend

---
+++ client/src/app/services/manage-inventory-items.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+
+import { ManageInventoryItemsService } from './manage-inventory-items.service';
+
+describe('ManageInventoryItemsService', () => {
+  let service: ManageInventoryItemsService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(ManageInventoryItemsService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
---
+++ client/src/app/services/manage-inventory-items.service.ts
@@ -0,0 +1,18 @@
+import { env } from 'src/environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+// Connection with the backend
+const BASEURL = env.api_root;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ManageInventoryItemsService {
+  constructor(private http: HttpClient) {}
+
+  getAllItems(): Observable<any> {
+    return this.http.get(`${BASEURL}/item/`);
+  }
+}
