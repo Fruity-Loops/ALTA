a5b6efb70425ecdf2f3a895b5274cc2eaaeb0931
Robert <robertbeaudenon@gmail.com>
2020-11-25 16:26:00 -0500

    feat: setting up the scheduler that will trigger the update of collection every minute

--- server/inventory_item/apps.py
+++ server/inventory_item/apps.py
@@ -3,3 +3,9 @@ from django.apps import AppConfig
 
 class ItemsConfig(AppConfig):
     name = 'inventory_item'
+
+    # To let Django know it needs to kick off our updater on startup
+    # overwrite the AppConfig.ready() method.
+    def ready(self):
+        from . import updater
+        updater.start()
---
+++ server/inventory_item/updater.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from django_server.load_csv_to_db import main
+
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(main, 'interval', minutes=1)
+    scheduler.start()
