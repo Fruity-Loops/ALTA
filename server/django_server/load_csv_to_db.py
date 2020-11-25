7c99363cc108dd04c45bf2f983d9675a39746b51
Robert <robertbeaudenon@gmail.com>
2020-11-25 16:24:10 -0500

    feat: adding an update function to update the data in the collection

--- server/django_server/load_csv_to_db.py
+++ server/django_server/load_csv_to_db.py
@@ -1,3 +1,4 @@
+import os
 import pymongo as pym
 import pandas as pd
 
@@ -41,3 +42,24 @@ def populate_items(csv_file, collection_name):
             # If the error is different to duplicate key entry
             if error.details["writeErrors"][i]["code"] != 11000:
                 print("An error occured")
+
+
+def update_items(csv_file, collection_name):
+    collection = get_collection(collection_name)
+    dict_of_records = clean_data(csv_file)
+
+    try:
+        for document in dict_of_records:
+            collection.update({'_id': document["_id"]}, document, upsert=True)
+        print("Data Refreshed")
+    except pym.errors.BulkWriteError as error:
+        print(error)
+
+
+def main(is_initializing=False):
+    current_path = os.path.dirname(__file__)
+    csv = os.path.join(current_path, "dummyData.csv")
+    if is_initializing:
+        populate_items(csv, "inventory_item_item")
+    else:
+        update_items(csv, "inventory_item_item")
