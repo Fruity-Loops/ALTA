506d441b869951d5db0d112b32c6ececfd161d22
Robert <robertbeaudenon@gmail.com>
2020-11-27 11:45:36 -0500

    feat: listened to scheduler events and storing jobs in collection jobs

--- server/inventory_item/updater.py
+++ server/inventory_item/updater.py
@@ -1,14 +1,47 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.mongodb import MongoDBJobStore
+from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 from django_server.load_csv_to_db import main
+import pymongo as pym
 
-# This will get you a BackgroundScheduler with a MemoryJobStore named “default”
-# and a ThreadPoolExecutor named “default” with a default maximum thread count of 10.
-scheduler = BackgroundScheduler()
+
+# Making connection to mongoclient
+client = pym.MongoClient("mongodb://localhost:27017/")
+
+job_stores = {
+    'default': MongoDBJobStore(database="alta_development",
+                               collection="jobs", client=client)
+}
+
+# This will get you a BackgroundScheduler with a MongoDBJobStore
+# named “default” to replace the default one  and a ThreadPoolExecutor
+# named “default” with a default maximum thread count of 10.
+scheduler = BackgroundScheduler(jobstores=job_stores)
 
 
 def start():
+    """
+    starts the scheduler
+    all the jobs that are stored in the jobs 
+    collection will be retrieved
+    """
     scheduler.start()
     print_all_job()
+
+
+def scheduler_listener(event):
+    """
+    :paramScheduler events
+    are fired on certain occasions, and may carry additional information
+    in them concerning the details of that particular event. It is possible
+    to listen to only particular types of events by giving the appropriate mask argument
+    to add_listener(), OR’ing the different constants together. The listener callable is
+    called with one argument, the event object.
+    """
+    if event.exception:
+        print('The job crashed :(')
+    else:
+        print('The job worked :)')
 
 
 def start_new_job(job_id, time):
@@ -18,9 +51,9 @@ def start_new_job(job_id, time):
 
 
 def print_all_job():
-    print("#############")
+    print("######ALL JOBS#######")
     scheduler.print_jobs()
-    print("#############")
+    print("#####################")
 
 
 def get_specific_job(id):
@@ -28,3 +61,6 @@ def get_specific_job(id):
     print("#############")
     print("###" + str(id) + "### " + str(job))
     print("#############")
+
+
+scheduler.add_listener(scheduler_listener, EVENT_JOB_EXECUTED | EVENT_JOB_ERROR)
