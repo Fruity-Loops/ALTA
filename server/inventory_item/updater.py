7ba7007a7e5b3c6e7133ca9b946a28be50b57699
Robert <robertbeaudenon@gmail.com>
2020-11-27 10:43:36 -0500

    feat: triggered a job specific to an organization that was just created

--- server/inventory_item/updater.py
+++ server/inventory_item/updater.py
@@ -1,8 +1,30 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from django_server.load_csv_to_db import main
 
+# This will get you a BackgroundScheduler with a MemoryJobStore named “default”
+# and a ThreadPoolExecutor named “default” with a default maximum thread count of 10.
+scheduler = BackgroundScheduler()
+
 
 def start():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(main, 'interval', minutes=1)
     scheduler.start()
+    print_all_job()
+
+
+def start_new_job(job_id, time):
+    scheduler.add_job(main, 'interval', minutes=time, id=job_id)
+    print_all_job()
+    get_specific_job(job_id)
+
+
+def print_all_job():
+    print("#############")
+    scheduler.print_jobs()
+    print("#############")
+
+
+def get_specific_job(id):
+    job = scheduler.get_job(id)
+    print("#############")
+    print("###" + str(id) + "### " + str(job))
+    print("#############")
--- server/organization/models.py
+++ server/organization/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Organization(models.Model):
     org_id = models.AutoField(primary_key=True)
     org_name = models.CharField(max_length=256, unique=True)
     status = models.BooleanField(default=False)
+    inventory_items_refresh_job = models.IntegerField(default=1)
+
+
+@receiver(post_save, sender=Organization)
+def execute_job(sender, instance, **kwargs):
+    print('post save callback')
+    start_new_job(instance.org_name, instance.inventory_items_refresh_job)
