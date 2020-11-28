e1d732e001015405664f7434fed15a6ca374c3fb
Robert <robertbeaudenon@gmail.com>
2020-11-28 00:32:47 -0500

    refactor: updated file based on pr review

--- server/django_server/load_csv_to_db.py
+++ server/django_server/load_csv_to_db.py
@@ -14,7 +14,7 @@ def get_collection(collection_name):
     return database[collection_name]
 
 
-def clean_data(csv_file, dummy_org_id):
+def clean_data(csv_file, org_id):
     dataframe = pd.read_csv(csv_file)
 
     # Renaming column to be compatible with models fields ( which doesn't contain spaces'
@@ -28,14 +28,14 @@ def clean_data(csv_file, dummy_org_id):
     dataframe = dataframe.dropna(subset=['_id'])
 
     # TODO : remove that when centralized db implemented
-    dataframe["organization"] = dummy_org_id
+    dataframe["organization"] = org_id
 
     return dataframe.to_dict('records')
 
 
-def populate_items(csv_file, collection_name, dummy_org_id):
+def populate_items(csv_file, collection_name, org_id):
     collection = get_collection(collection_name)
-    dict_of_records = clean_data(csv_file, dummy_org_id)
+    dict_of_records = clean_data(csv_file, org_id)
 
     try:
         collection.insert_many(dict_of_records)
@@ -48,9 +48,9 @@ def populate_items(csv_file, collection_name, dummy_org_id):
                 print("An error occured")
 
 
-def update_items(csv_file, collection_name, org_name, dummy_org_id):
+def update_items(csv_file, collection_name, org_name, org_id):
     collection = get_collection(collection_name)
-    dict_of_records = clean_data(csv_file, dummy_org_id)
+    dict_of_records = clean_data(csv_file, org_id)
 
     try:
         for document in dict_of_records:
@@ -60,44 +60,7 @@ def update_items(csv_file, collection_name, org_name, dummy_org_id):
         print(error)
 
 
-# TODO: delete that when we have our centralized db
-# def create_dummy_organization():
-#     from user_account.models import CustomUser
-#     from organization.models import Organization
-#     from rest_framework.test import APITestCase
-#     from rest_framework.test import APIClient
-#
-#     client = APIClient()
-#
-#     # Create a user that could be making the  request
-#     try:
-#         system_admin = CustomUser.objects.get(user_name="test")
-#         client.force_authenticate(user=system_admin)
-#     except CustomUser.DoesNotExist:
-#         system_admin = CustomUser.objects.create(
-#             id=random.randint(600, 700),
-#             user_name='test',
-#             email='test@email.com',
-#             password='password1',
-#             first_name='system1',
-#             last_name='admin1',
-#             role='SA',
-#             is_active=True)
-#         client.force_authenticate(user=system_admin)
-#
-#     # Create an organization that could be linked to the inventory items
-#     try:
-#         organization = Organization.objects.get(org_name='test_organization')
-#         return organization.org_id
-#     except Organization.DoesNotExist:
-#         data = {'org_name': 'test_organization'}
-#         response = client.post("/organization/", data)
-#         return response.data["org_id"]
-
-
 def main(org_name, is_initializing=False):
-    # TODO remove that its just for demo purposes
-    # dummy_org_id = create_dummy_organization()
     current_path = os.path.dirname(__file__)
     csv = os.path.join(current_path, "test_organization.csv")
     if is_initializing:
