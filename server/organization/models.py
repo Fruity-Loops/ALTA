6a2ef4ec6791d0aa30314fd4ef20ba0b7f923f14
Khaled Badran <badrankhaled97@gmail.com>
2020-11-29 19:13:42 -0500

    fix: Add a default value for organization address

--- server/organization/models.py
+++ server/organization/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Organization(models.Model):
     org_id = models.AutoField(primary_key=True)
     org_name = models.CharField(max_length=256, unique=True)
-    address = models.CharField(max_length=256)
+    address = models.CharField(max_length=256, default="Montreal")
     status = models.BooleanField(default=False)
