0d31978ac94863558043587435c7e3ff00d23cab
Dominik Ludera <domludera@gmail.com>
2020-11-29 20:59:19 -0500

    fix: added test

--- server/inventory_item/tests.py
+++ server/inventory_item/tests.py
@@ -31,3 +31,10 @@ class ItemTestCase(APITestCase):
         response = self.client.get("/item/")
         self.assertEqual(response.status_code,
                          status.HTTP_200_OK)
+        
+    def test_get_all_inventory_items(self):
+        """ Obtaining all items """
+        self.client.force_authenticate(user=self.system_admin)
+        response = self.client.get("/item/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 4)
