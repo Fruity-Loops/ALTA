4f0223e6e12226114d4a675c2afcdbc6c7301412
Robert <robertbeaudenon@gmail.com>
2020-12-04 04:03:05 +0200

    test: added dependency in testing inventory item component

--- client/src/app/components/manage-inventory-items/manage-inventory-items.component.spec.ts
+++ client/src/app/components/manage-inventory-items/manage-inventory-items.component.spec.ts
@@ -1,8 +1,9 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {ManageInventoryItemsService} from 'src/app/services/manage-inventory-items.service';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterTestingModule} from '@angular/router/testing';
-import {ManageInventoryItemsComponent} from './manage-inventory-items.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ManageInventoryItemsService } from 'src/app/services/manage-inventory-items.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ManageInventoryItemsComponent } from './manage-inventory-items.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('ManageInventoryItemsComponent', () => {
   let component: ManageInventoryItemsComponent;
@@ -12,7 +13,7 @@ describe('ManageInventoryItemsComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ManageInventoryItemsComponent],
-      providers: [ManageInventoryItemsService],
+      providers: [FormBuilder, { provide: ManageInventoryItemsService }],
       imports: [HttpClientModule, RouterTestingModule],
     });
 
