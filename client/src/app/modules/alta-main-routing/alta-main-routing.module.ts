9348950903c2f6e85705395baa6db371f0a12f86
Khaled Badran <badrankhaled97@gmail.com>
2020-12-07 16:21:38 -0500

    feat: add audit templates component

--- client/src/app/modules/alta-main-routing/alta-main-routing.module.ts
+++ client/src/app/modules/alta-main-routing/alta-main-routing.module.ts
@@ -8,6 +8,8 @@ import { EmployeeSettingsComponent } from '../../components/employee-settings/em
 import { ClientGridviewComponent } from '../../components/client-gridview/client-gridview.component';
 import { CreateMemberComponent } from '../../components/create-member/create-member.component';
 import { ManageInventoryItemsComponent } from 'src/app/components/manage-inventory-items/manage-inventory-items.component';
+import { AuditTemplateComponent } from '../../components/audit-template/audit-template.component';
+
 
 const routes: Routes = [
   {
@@ -29,6 +31,7 @@ const routes: Routes = [
       { path: 'settings', component: EmployeeSettingsComponent },
       { path: 'sa-settings', component: EmployeeSettingsComponent },
       { path: 'manage-items', component: ManageInventoryItemsComponent },
+      { path: 'template', component: AuditTemplateComponent },
     ],
   },
 ];
