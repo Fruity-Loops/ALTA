e5e3e251ef79d6ff78941087f4c70c9d81cb0c8b
Robert <robertbeaudenon@gmail.com>
2020-12-10 22:08:50 +0200

    feat: adding link to related code article

--- server/inventory_item/apps.py
+++ server/inventory_item/apps.py
@@ -9,5 +9,8 @@ class ItemsConfig(AppConfig):
     def ready(self):
         #  Due to the intricacies of inheritance, any imports for this
         #  override must be located within be body of the ready() method
+        #  related article:
+        # https://medium.com/@kevin.michael.horan
+        # /scheduling-tasks-in-django-with-the-advanced-python-scheduler-663f17e868e6
         from . import updater
         updater.start()
