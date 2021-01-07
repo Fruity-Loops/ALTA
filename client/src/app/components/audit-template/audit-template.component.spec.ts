31867844c1ffcc017d589504275aa4a5dd50a2d8
Neeham <Neehamk@gmail.com>
2021-01-06 19:40:02 -0500

    fix: removal of flex layout

--- client/src/app/components/audit-template/audit-template.component.spec.ts
+++ client/src/app/components/audit-template/audit-template.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { RouterTestingModule } from "@angular/router/testing";
+import { RouterTestingModule } from '@angular/router/testing';
 import { AuditTemplateComponent } from './audit-template.component';
 
 describe('AuditTemplateComponent', () => {
