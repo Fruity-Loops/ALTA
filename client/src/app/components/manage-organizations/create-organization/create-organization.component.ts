805c9274a93ae63e3ce37a0b75c0cb2191cae11e
Nicholas Nagy <ygannicholas@gmail.com>
2021-01-12 17:50:48 -0500

    feat: Add template for modifying organization

--- client/src/app/components/manage-organizations/create-organization/create-organization.component.ts
+++ client/src/app/components/manage-organizations/create-organization/create-organization.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrganizationComponent implements OnInit {
 
+  activeStates = ["active", "disabled"];
+
   constructor() { }
 
   ngOnInit(): void {
