c93940c68b1bac578e205b18cf45cdbf1d9136ef
Amanda-Kolopa <amandakolopa@gmail.com>
2021-01-15 11:44:41 -0500

    test: fix frontend tests for AssignSK and ManageSKDesignation

--- client/src/app/components/assign-stock-keepers/assign-stock-keepers.component.spec.ts
+++ client/src/app/components/assign-stock-keepers/assign-stock-keepers.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AssignStockKeepersComponent } from './assign-stock-keepers.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ManageMembersService } from 'src/app/services/manage-members.service';
+import { ManageAuditsService } from 'src/app/services/manage-audits.service';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('AssignStockKeepersComponent', () => {
   let component: AssignStockKeepersComponent;
   let fixture: ComponentFixture<AssignStockKeepersComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AssignStockKeepersComponent ]
-    })
-    .compileComponents();
-  });
+  let service: ManageMembersService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AssignStockKeepersComponent],
+      providers: [
+        FormBuilder,
+        { provide: ManageMembersService },
+        { provide: ManageAuditsService }
+      ],
+      imports: [HttpClientModule, RouterTestingModule, MatDialogModule ],
+    });
+
     fixture = TestBed.createComponent(AssignStockKeepersComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(ManageMembersService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Assign Stock-Keepers Component', () => {
     expect(component).toBeTruthy();
   });
 });
--- client/src/app/components/manage-stock-keepers-designation/manage-stock-keepers-designation.component.spec.ts
+++ client/src/app/components/manage-stock-keepers-designation/manage-stock-keepers-designation.component.spec.ts
@@ -1,25 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ManageStockKeepersDesignationComponent } from './manage-stock-keepers-designation.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ManageMembersService } from 'src/app/services/manage-members.service';
+import { ManageAuditsService } from 'src/app/services/manage-audits.service';
+import { FormBuilder } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
 
 describe('ManageStockKeepersDesignationComponent', () => {
   let component: ManageStockKeepersDesignationComponent;
   let fixture: ComponentFixture<ManageStockKeepersDesignationComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ManageStockKeepersDesignationComponent ]
-    })
-    .compileComponents();
-  });
+  let service: ManageAuditsService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ManageStockKeepersDesignationComponent],
+      providers: [
+        FormBuilder,
+        { provide: ManageMembersService },
+        { provide: ManageAuditsService }
+      ],
+      imports: [HttpClientModule, RouterTestingModule, MatDialogModule ],
+    });
+
     fixture = TestBed.createComponent(ManageStockKeepersDesignationComponent);
     component = fixture.componentInstance;
+    service = TestBed.inject(ManageAuditsService);
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Manage Stock-Keepers Designation Component', () => {
     expect(component).toBeTruthy();
   });
 });
