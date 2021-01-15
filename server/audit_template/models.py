9953af385899f4f08ef6bdf25bd7f37e31022bb2
Avery <averyndersingh@gmail.com>
2021-01-15 17:47:50 -0500

    fix: fixed audit_template failed ID creation
    Previously, when creating audit templates it would not work as they were created with no ID. The reason for this is because the correct migrations file was missing / the schemas were wrong because the djongo and django

--- server/audit_template/models.py
+++ server/audit_template/models.py
@@ -1,4 +1,5 @@
-from djongo import models
+from django.db import models
+from djongo.models.fields import JSONField
 from organization.models import Organization
 
 
@@ -7,13 +8,13 @@ class AuditTemplate(models.Model):
     template_id = models.AutoField(primary_key=True)
     author = models.CharField(max_length=512)
     title = models.CharField(max_length=256, blank=True)
-    location = models.JSONField(null=True, blank=True)
-    plant = models.JSONField(null=True, blank=True)
-    zones = models.JSONField(null=True, blank=True)
-    aisles = models.JSONField(null=True, blank=True)
-    bins = models.JSONField(null=True, blank=True)
-    part_number = models.JSONField(null=True, blank=True)
-    serial_number = models.JSONField(null=True, blank=True)
+    location = JSONField(null=True, blank=True)
+    plant = JSONField(null=True, blank=True)
+    zones = JSONField(null=True, blank=True)
+    aisles = JSONField(null=True, blank=True)
+    bins = JSONField(null=True, blank=True)
+    part_number = JSONField(null=True, blank=True)
+    serial_number = JSONField(null=True, blank=True)
     description = models.TextField(blank=True)
     calendar_date = models.CharField(max_length=100)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, blank=True, null=True)
