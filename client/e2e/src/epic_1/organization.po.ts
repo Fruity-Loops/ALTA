508c83e6d46ddc32e8a2df1bc7ad9e9f49e5a9b0
Avery <averyndersingh@gmail.com>
2021-01-18 03:50:34 -0500

    test: fixed AT 1.3: A,B,C
    This also adds the appropriate html ids within the client side to have access to them when running the test

---
+++ client/e2e/src/epic_1/organization.po.ts
@@ -0,0 +1,62 @@
+import { browser, by, element, ElementFinder } from 'protractor';
+
+export class OrganizationPage {
+  navigateTo(): Promise<unknown> {
+    const route = `${browser.baseUrl}/#/manage-organizations`;
+    return browser.get(route) as Promise<unknown>;
+  }
+  getAddOrganizationButton(): ElementFinder {
+    return element(by.id('createOrg'));
+  }
+
+  getOrganizationTable(): ElementFinder {
+    return element(by.tagName('table'));
+  }
+
+  getOrganizationNameDialogueField(): ElementFinder {
+    return element(by.id('EditOrgName'));
+  }
+
+  getOrganizationLocationDialogueField(): ElementFinder {
+    return element(by.id('EditOrgLocation'));
+  }
+
+  getOrganizationNameColumn(organizationName): ElementFinder {
+    return element(by.cssContainingText(
+      '.cell_org_name',
+      organizationName)
+    );
+  }
+
+  getOrganizationStatusColumn(organizationName): ElementFinder {
+    return element(by.id(organizationName + ' status'));
+  }
+
+  getOrgSettings(name): ElementFinder {
+    return element(by.id('settings ' + name));
+  }
+
+  getOrgEditButton(): ElementFinder {
+    return element(by.id('editOrganizationButton'));
+  }
+
+  getOrgStatusDropDown(): ElementFinder {
+    return element(by.id('orgStatusDropDown'));
+  }
+
+  getDisabeledStatus(): ElementFinder {
+    return element(by.id('Disabled'));
+  }
+
+  getSaveOrganizationButton(): ElementFinder {
+    return element(by.id('saveEditOrganizationButton'));
+  }
+
+  getDisableOrgNameInput(): ElementFinder {
+    return element(by.id('disableOrgNameInput'));
+  }
+
+  getDisableOrgConfirmationButton(): ElementFinder {
+    return element(by.id('confirmDisableOrg'));
+  }
+}
