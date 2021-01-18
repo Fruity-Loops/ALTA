4d6943ce6f8259e773f1063eeec670a1152c38b5
Robert <robertbeaudenon@gmail.com>
2021-01-18 19:35:34 +0000

    feat: adding template id to keep track of the template in audit

--- server/audit/models.py
+++ server/audit/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from inventory_item.models import Item
+from audit_template.models import AuditTemplate
 
 
 class Audit(models.Model):
     audit_id = models.AutoField(primary_key=True)
     inventory_items = models.ManyToManyField(Item)
+    template_id = models.ForeignKey(AuditTemplate, on_delete=models.CASCADE, blank=True, null=True)
