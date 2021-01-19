655de630b238a3c9378370b6277388e44c6861c7
Robert <robertbeaudenon@gmail.com>
2021-01-19 13:03:00 +0000

    refactor: creating a function to split the tasks

--- server/inventory_item/updater.py
+++ server/inventory_item/updater.py
@@ -4,6 +4,7 @@ from apscheduler.events import EVENT_JOB_EXECUTED, EVENT_JOB_ERROR
 import pymongo as pym
 from django_server.load_csv_to_db import main
 from audit.utils import create_audit
+from audit_template.models import AuditTemplate
 
 
 class Singleton(type):
@@ -77,11 +78,14 @@ def start_new_job(job_id, time):
     get_specific_job(job_id)
 
 
-def start_new_cron_job(template_id):
+def start_new_cron_job(template_id, date, time_zone):
     job_id = "template_" + str(template_id)
+    template = AuditTemplate.objects.get(template_id=template_id)
+    kwargs = get_job_queries(template.repeat_every, template.on_day, template.for_month)
 
-    scheduler.add_job(create_audit, 'cron', timezone=time_zone, **kwargs, id=job_id,
-                      args=(template_id,), replace_existing=True)
+    # Each Job will trigger the function create_audit(template_id) at specific interval
+    scheduler.add_job(create_audit, 'cron', start_date=date, timezone=time_zone, **kwargs, hour=7,
+                      id=job_id, args=(template_id,), replace_existing=True)
     print_all_job()
     get_specific_job(job_id)
 
@@ -105,3 +109,33 @@ def get_specific_job(job_id):
     print("#############")
     print("###" + str(job_id) + "### " + str(job))
     print("#############")
+
+
+def get_job_queries(repetition, day_of_week, months):
+    week_query = ''
+    for day in day_of_week:
+        if repetition == 'all':
+            week_query += day + ','
+        else:
+            if repetition == '1':
+                week_query += '1st ' + day + ','
+            elif repetition == '2':
+                week_query += '2nd ' + day + ','
+            elif repetition == '3':
+                week_query += '3rd ' + day + ','
+            elif repetition == 'last':
+                week_query += '4th ' + day + ','
+
+    month_query = ''
+    for m in months:
+        month_query += m + ','
+
+    kwargs = {}
+    if repetition == 'all':
+        kwargs['day_of_week'] = week_query[:-1]
+    else:
+        kwargs['day'] = week_query[:-1]
+
+    kwargs['month'] = month_query[:-1]
+
+    return kwargs
