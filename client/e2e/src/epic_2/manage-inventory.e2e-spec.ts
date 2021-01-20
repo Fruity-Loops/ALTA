8d9b41140604dd3c861b4bacf3ea6a9327bfba97
Avery <averyndersingh@gmail.com>
2021-01-19 19:19:17 -0500

    test: added AT 2.3, 2.5

---
+++ client/e2e/src/epic_2/manage-inventory.e2e-spec.ts
@@ -0,0 +1,94 @@
+import { browser, ExpectedConditions } from 'protractor';
+import { Navigation } from '../navigation.po';
+import { InventoryItemsPage } from './inventory.po';
+import { Login, Logout } from '../login.po'
+/**
+ * Supports Acceptance Tests:
+ * AT-2.3: 
+ * https://github.com/fruity-loops/alta/issues/46
+ */
+describe('AT-2.3: Inventory Manager can acess their organization\'s inventory', () => {
+    const inventoryPage: InventoryItemsPage = new InventoryItemsPage();
+    const nav: Navigation = new Navigation();
+  
+    /**
+     * Login as an Inventory Manager
+     */
+    beforeAll(function () {
+      const loginPage = new Login();
+      loginPage.login_as('im@test.com', false);
+    });
+  
+    /**
+     * Logout
+     */
+    afterAll(function () {
+      const logoutPage = new Logout();
+      logoutPage.logout();
+    });
+  
+    /**
+     * i) The user clicks on Inventory.
+     */
+    it('should go to the organization\'s inventory page', () => {
+      nav.inventoryOption().click();
+      browser.wait(ExpectedConditions.urlContains('manage-items'), 5000);
+    });
+  
+    /**
+     * i) The table is displayed
+     * ii) The data from the table is checked against the fixtures.
+     */
+    it('should show inventory items from the organization', () => {
+        expect(inventoryPage.getInventoryTable().isDisplayed()).toBeTruthy();
+        expect(inventoryPage.getCheckBoxForID(12731369).isDisplayed()).toBeTruthy();
+        expect(inventoryPage.getCheckBoxForID(12752842).isDisplayed()).toBeTruthy();
+    });
+  });
+
+  /**
+ * Supports Acceptance Tests:
+ * AT-2.5: 
+ * https://github.com/fruity-loops/alta/issues/48
+ */
+describe('AT-2.3: Inventory Manager can search through their organization\'s inventory', () => {
+  const inventoryPage: InventoryItemsPage = new InventoryItemsPage();
+  const nav: Navigation = new Navigation();
+
+  /**
+   * Login as an Inventory Manager
+   */
+  beforeAll(function () {
+    const loginPage = new Login();
+    loginPage.login_as('im@test.com', false);
+  });
+
+  /**
+   * Logout
+   */
+  afterAll(function () {
+    const logoutPage = new Logout();
+    logoutPage.logout();
+  });
+
+  /**
+   * i) The user clicks on Inventory.
+   */
+  it('should go to the organization\'s inventory page', () => {
+    nav.inventoryOption().click();
+    browser.wait(ExpectedConditions.urlContains('manage-items'), 5000);
+  });
+
+  /**
+   * i) The table is displayed
+   * ii) The search data is filled in
+   * iii) The data from the table is changed
+   */
+  it('should show inventory items searched for', () => {
+      expect(inventoryPage.getInventoryTable().isDisplayed()).toBeTruthy();
+      inventoryPage.getSearchInputField().sendKeys('False');
+      inventoryPage.getSearchButton().click();
+      expect(inventoryPage.getCheckBoxForID(12731369).isDisplayed()).toBeTruthy();
+      expect(browser.isElementPresent(inventoryPage.getCheckBoxForID(12752842))).toBeFalsy();
+  });
+});
