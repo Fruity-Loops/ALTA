abd8a249be838ec9b1388bb5ecd64898e731d29e
makchamp <makramadaime@hotmail.com>
2021-01-27 21:04:01 -0500

    feat: Showing username in profile popover Should maybe show actual name or more infromation instead later

--- mobile/src/app/components/toolbar/popovers/profile-popover-menu/profile-popover-menu.component.ts
+++ mobile/src/app/components/toolbar/popovers/profile-popover-menu/profile-popover-menu.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-import { PopoverController } from '@ionic/angular';
+import { PopoverController, NavParams } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile-popover-menu',
   templateUrl: './profile-popover-menu.component.html',
   styleUrls: ['./profile-popover-menu.component.scss'],
+  providers: [NavParams],
 })
 export class ProfilePopoverMenuComponent implements OnInit {
+  loggedInUser: string;
 
   constructor(
     private router: Router,
+    public navParams: NavParams,
     private authService: AuthService,
-    private popoverController: PopoverController) { }
+    private popoverController: PopoverController) {
+
+    this.loggedInUser = this.navParams.get('loggedInUser');
+  }
 
   ngOnInit() { }
 
--- mobile/src/app/components/toolbar/toolbar.component.ts
+++ mobile/src/app/components/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PopoverController } from '@ionic/angular';
 import { ProfilePopoverMenuComponent } from 'src/app/components/toolbar/popovers/profile-popover-menu/profile-popover-menu.component';
 import { NotificationPopoverMenuComponent } from 'src/app/components/toolbar/popovers/notification-popover-menu/notification-popover-menu.component';
+import { fetchLoggedInUser } from 'src/app/services/cache';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,17 +10,20 @@ import { NotificationPopoverMenuComponent } from 'src/app/components/toolbar/pop
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  loggedInUser: string;
   constructor(
     public popoverController: PopoverController
-    ) { }
+  ) { }
 
   ngOnInit() { }
 
   async presentProfilePopover(ev: any) {
+    const { username } = await fetchLoggedInUser();
     const popover = await this.popoverController.create({
       component: ProfilePopoverMenuComponent,
       event: ev,
       translucent: true,
+      componentProps: { loggedInUser: username },
     });
     return await popover.present();
   }
