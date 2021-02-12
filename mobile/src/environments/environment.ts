d491590c4c950db274d9b9e81f50706b7d79d766
makchamp <makramadaime@hotmail.com>
2021-02-12 13:57:48 -0500

    config: Ignoring db populate script from sonar and pylint + gitignore plugins folder

--- mobile/src/environments/environment.ts
+++ mobile/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const env = {
   production: false,
-  api_root: 'http://b087606fea8b.ngrok.io',
+  api_root: 'http://localhost:8000',
   open_registration: false, // Set to 'true' to allow registartion from the greeting page
 };
 
