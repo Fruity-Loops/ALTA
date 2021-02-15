5fe8d3e0ad359b4d646b26a4d6e4caf50a0e75b3
Nicholas Nagy <ygannicholas@gmail.com>
2021-02-15 18:43:58 -0500

    fix: Testing providers/dependencies for audit template

--- client/src/app/services/audits/audit-template.service.spec.ts
+++ client/src/app/services/audits/audit-template.service.spec.ts
@@ -1,14 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AuditTemplateService } from './audit-template.service';
+import {ManageAuditsService} from './manage-audits.service';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('AuditTemplateService', () => {
   let service: AuditTemplateService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      providers: [AuditTemplateService]
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [AuditTemplateService, ManageAuditsService]
     });
     service = TestBed.inject(AuditTemplateService);
   });
