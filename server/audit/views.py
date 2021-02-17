235e72e48f0cf2212336e20d1097778c3e543108
Avery <averyndersingh@gmail.com>
2021-02-17 15:53:39 -0500

    chore: replacing dispatch contents with super call

--- server/audit/views.py
+++ server/audit/views.py
@@ -144,35 +144,10 @@ class RecordViewSet(viewsets.ModelViewSet):
         return serializer_class(*args, **kwargs)
 
     def dispatch(self, request, *args, **kwargs):
-        """
-        `.dispatch()` is pretty much the same as Django's regular dispatch,
-        but with extra hooks for startup, finalize, and exception handling.
-        Overriding this message to set the accuracy after updates and creations
-        """
-        self.args = args
-        self.kwargs = kwargs
-        request = self.initialize_request(request, *args, **kwargs)
-        self.request = request
-        self.headers = self.default_response_headers  # deprecate?
-
-        try:
-            self.initial(request, *args, **kwargs)
-
-            # Get the appropriate handler method
-            if request.method.lower() in self.http_method_names:
-                handler = getattr(self, request.method.lower(),
-                                  self.http_method_not_allowed)
-            else:
-                handler = self.http_method_not_allowed
-
-            response = handler(request, *args, **kwargs)
-
-        except Exception as exc:
-            response = self.handle_exception(exc)
+        response = super().dispatch(request, *args, **kwargs)
         if response.data:
             if 'audit' in response.data:
                 set_audit_accuracy(response.data['audit'])
-        self.response = self.finalize_response(request, response, *args, **kwargs)
         return self.response
 
     @action(detail=False, methods=['GET'], name='Get Completed Items')
