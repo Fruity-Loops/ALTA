f02ae9084840d18a9a84a0e938c7857fd924d94a
Robert <robertbeaudenon@gmail.com>
2021-03-03 19:50:02 -0500

    feat: adding organization as a param in the request

--- client/src/app/services/dashboard/dashboard.service.ts
+++ client/src/app/services/dashboard/dashboard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { env } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+// Connection with the backend
+const BASEURL = env.api_root;
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashboardService {
+  orgId: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.orgId = '';
+  }
+
+  getRecommendations(params: HttpParams): Observable<any> {
+    return this.http.get(`${BASEURL}/recommendation/`, { params });
+  }
 }
