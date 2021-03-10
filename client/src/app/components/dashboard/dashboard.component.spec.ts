d2d48784b3e710434c0b3c67e9d6d9499d99d008
Daphné <daphne.augier@gmail.com>
2021-03-09 23:16:42 -0500

    adding basic data for table on the dashboard page

--- client/src/app/components/dashboard/dashboard.component.spec.ts
+++ client/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,25 +1,35 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {DashboardComponent} from './dashboard.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ManageInventoryItemsService } from 'src/app/services/inventory-items/manage-inventory-items.service';
+import { ManageAuditsService } from 'src/app/services/audits/manage-audits.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
+import { DashboardComponent } from './dashboard.component';
+import { FormBuilder } from '@angular/forms';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DashboardComponent]
-    })
-      .compileComponents();
+    // @ts-ignore
+    let service: ManageInventoryItemsService;
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [DashboardComponent],
+        providers: [
+          FormBuilder,
+          { provide: ManageAuditsService },
+        ],
+        imports: [HttpClientModule, RouterTestingModule],
+      });
+  
+      fixture = TestBed.createComponent(DashboardComponent);
+      component = fixture.componentInstance;
+      service = TestBed.inject(ManageInventoryItemsService);
+      fixture.detectChanges();
+    });
+  
+    it('should create Dashboard Component', () => {
+      expect(component).toBeTruthy();
+    });
   });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create component', () => {
-    expect(component).toBeTruthy();
-  });
-
-});
+  
