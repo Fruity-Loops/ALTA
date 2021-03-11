3c96b344b8ff6f375ca00eab05c21c5441a3bd1e
Neeham <Neehamk@gmail.com>
2021-03-10 22:23:08 -0500

    feat: added table skeleton for for bin service

--- client/src/app/components/dashboard/dashboard.component.spec.ts
+++ client/src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,35 +1,35 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ManageInventoryItemsService } from 'src/app/services/inventory-items/manage-inventory-items.service';
-import { ManageAuditsService } from 'src/app/services/audits/manage-audits.service';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
-import { DashboardComponent } from './dashboard.component';
-import { FormBuilder } from '@angular/forms';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ManageInventoryItemsService} from 'src/app/services/inventory-items/manage-inventory-items.service';
+import {ManageAuditsService} from 'src/app/services/audits/manage-audits.service';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterTestingModule} from '@angular/router/testing';
+import {DashboardComponent} from './dashboard.component';
+import {FormBuilder} from '@angular/forms';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
-    // @ts-ignore
-    let service: ManageInventoryItemsService;
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        declarations: [DashboardComponent],
-        providers: [
-          FormBuilder,
-          { provide: ManageAuditsService },
-        ],
-        imports: [HttpClientModule, RouterTestingModule],
-      });
-  
-      fixture = TestBed.createComponent(DashboardComponent);
-      component = fixture.componentInstance;
-      service = TestBed.inject(ManageInventoryItemsService);
-      fixture.detectChanges();
+  // @ts-ignore
+  let service: ManageInventoryItemsService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [DashboardComponent],
+      providers: [
+        FormBuilder,
+        {provide: ManageAuditsService},
+      ],
+      imports: [HttpClientModule, RouterTestingModule],
     });
-  
-    it('should create Dashboard Component', () => {
-      expect(component).toBeTruthy();
-    });
+
+    fixture = TestBed.createComponent(DashboardComponent);
+    component = fixture.componentInstance;
+    service = TestBed.inject(ManageInventoryItemsService);
+    fixture.detectChanges();
   });
-  
+
+  it('should create Dashboard Component', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {OnInit, Component, ViewChild} from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+// TODO: Get service for bins to populate table data.
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +10,34 @@ import {Component, OnInit, ViewChild} from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {
+
+  dataSource: MatTableDataSource<any>;
+  audits: Array<any>;
+  displayedColumns: string[] = ['dateInitiated', 'ID', 'description', 'location', 'bin', 'initiatedBy', 'accuracy', 'status'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  @ViewChild(MatSort) sort: MatSort | undefined;
+
+  constructor(private bins: any) { // TODO: Replace any with classname of bin service
+    this.audits = new Array<any>();
+    this.dataSource = new MatTableDataSource<any>(this.audits);
+    this.bins.getAllClients().subscribe((audit: any) => {
+      this.retrieveData(audit);
+    });
   }
 
-  view = 'Dashboard';
+  retrieveData(audit: any): void {
+    audit.forEach((element: any) => {
+      this.audits.push(element);
+    });
+    console.log(this.audits);
+    // @ts-ignore
+    this.dataSource.paginator = this.paginator;
+    // @ts-ignore
+    this.dataSource.sort = this.sort;
+  }
 
   ngOnInit(): void {
   }
+
 }
