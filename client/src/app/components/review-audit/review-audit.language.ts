8e59f925051556eeae856c2dac01f6c935029f13
Nicholas Nagy <ygannicholas@gmail.com>
2021-04-04 19:04:04 -0400

    fix: Capitalise By and On as requested in Review Audit

--- client/src/app/components/review-audit/review-audit.language.ts
+++ client/src/app/components/review-audit/review-audit.language.ts
@@ -33,7 +33,7 @@ class ReviewAuditEnglish implements ReviewAuditLanguage {
       expand: 'Expand All', collapse: 'Collapse All', back: 'Go Back', discard: 'Discard', confirm: 'Confirm', cancel: 'Cancel'
     };
     this.tableHeaders = {
-      stock_keeper: 'Stock Keeper', bins: 'Bins', num_parts: 'Number of Parts', init_by: 'Initiated by', init_on: 'Initiated on'
+      stock_keeper: 'Stock Keeper', bins: 'Bins', num_parts: 'Number of Parts', init_by: 'Initiated By', init_on: 'Initiated On'
     };
   }
 }
