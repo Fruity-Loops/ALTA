69803138f3249e43c50c9e2d9891d51af89e8949
Nicholas Nagy <ygannicholas@gmail.com>
2021-04-08 12:06:19 -0400

    chore: remove duplicate import

--- client/src/app/modules/auth/auth.module.ts
+++ client/src/app/modules/auth/auth.module.ts
@@ -2,14 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {GreetingComponent} from '../../components/greeting/greeting.component';
-import {LoginComponent} from 'src/app/components/login/login.component';
+import {LoginComponent, ForgotCredentialsComponent} from 'src/app/components/login/login.component';
 import {CreateEmployeeComponent} from 'src/app/components/employees/employee-view/create-employee/create-employee.component';
 import {AuthService} from 'src/app/services/authentication/auth.service';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
 import {MatModule} from '../material/material-module';
 import {FlexModule} from '@angular/flex-layout';
-import {ForgotCredentialsComponent} from 'src/app/components/login/login.component';
 
 @NgModule({
   declarations: [GreetingComponent, LoginComponent, CreateEmployeeComponent, ForgotCredentialsComponent],
