bf265c7c2fe88556c8fff7edfaff48b17016e093
Daphné <daphne.augier@gmail.com>
2021-04-08 02:11:08 -0400

    test: added missing test files and 2 tests with it

---
+++ client/src/app/components/manage-organizations/organization-view/create-organization/create-organization.component.spec.ts
@@ -0,0 +1,50 @@
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ManageOrganizationsService} from 'src/app/services/organizations/manage-organizations.service';
+import {FormBuilder} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+
+import {CreateOrganizationComponent} from './create-organization.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import { AppModule } from 'src/app/app.module'
+
+describe('CreateOrganizationComponent', () => {
+  let component: CreateOrganizationComponent;
+  let fixture: ComponentFixture<CreateOrganizationComponent>;
+  // @ts-ignore
+  let organizationService: ManageOrganizationsService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CreateOrganizationComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule, MatDialogModule, AppModule],
+      providers: [
+        FormBuilder,
+        {
+          provide: ManageOrganizationsService,
+        },
+      ],
+    }).compileComponents();
+
+    organizationService = TestBed.inject(ManageOrganizationsService);
+    
+    fixture = TestBed.createComponent(CreateOrganizationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  // Test the submitQuery table
+  it('Call submitQuery', () => {
+    try {
+      component.submitQuery()
+    }
+    catch (err) {
+      console.error(err);
+    }
+  });
+
+});
