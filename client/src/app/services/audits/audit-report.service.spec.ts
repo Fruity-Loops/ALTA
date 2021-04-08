bd20b5a79ba3edfe5eddf5123f2c1f60e9f61f57
Amanda-Kolopa <amandakolopa@gmail.com>
2021-04-08 16:36:53 -0400

    fix: failing tests and UI improvements

--- client/src/app/services/audits/audit-report.service.spec.ts
+++ client/src/app/services/audits/audit-report.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { AuditReportService } from './audit-report.service';
 
 describe('AuditReportService', () => {
   let service: AuditReportService;
+  // @ts-ignore
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(AuditReportService);
   });
 
