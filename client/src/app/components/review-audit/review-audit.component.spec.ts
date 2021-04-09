f5f50d66d468a87e910dad41d8269667a26fff01
Avery <averyndersingh@gmail.com>
2021-04-09 01:09:32 -0400

    test: fix broken time test
    removed timezone from test

--- client/src/app/components/review-audit/review-audit.component.spec.ts
+++ client/src/app/components/review-audit/review-audit.component.spec.ts
@@ -68,7 +68,7 @@ describe('AssignStockKeepersComponent', () => {
         "bins": "C69",
         "numberOfParts": 1,
         "initiatedBy": "inventory manager",
-        "date": "4/8/2021 0:15",
+        "date": "4/8/2021 20:0",
         "location": "Florida"
       },
       {
@@ -76,7 +76,7 @@ describe('AssignStockKeepersComponent', () => {
         "bins": "C20",
         "numberOfParts": 1,
         "initiatedBy": "inventory manager",
-        "date": "4/8/2021 0:15",
+        "date": "4/8/2021 20:0",
         "location": "Florida"
       }
     ]
