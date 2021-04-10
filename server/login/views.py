977a89f312156a7d294c8c24bb0043ab3b82251d
Nicholas Nagy <ygannicholas@gmail.com>
2021-04-10 19:06:54 -0400

    chore: removing unused logging statements in login

--- server/login/views.py
+++ server/login/views.py
@@ -128,9 +128,6 @@ def save_new_pin(email, user):
     request.data = {'password': first_part + second_part + third_part}
     # For e2e purposes, uncomment this line:
     # request.data = {'password': 'password'}
-    print('\n\n--------------------------')
-    print(request.data)
-    print('--------------------------\n\n')
 
     request.user = email
     kwargs = {'partial': True, 'pk': user.id}
