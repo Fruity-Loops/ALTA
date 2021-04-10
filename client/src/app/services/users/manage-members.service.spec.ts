94f930ec13714d5a79d93e35ea3bc0965c486120
Avery <averyndersingh@gmail.com>
2021-04-10 03:17:14 -0400

    test: added error test case

--- client/src/app/services/users/manage-members.service.spec.ts
+++ client/src/app/services/users/manage-members.service.spec.ts
@@ -5,8 +5,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService, UserLocalStorage } from 'src/app/services/authentication/auth.service';
 import { env } from 'src/environments/environment';
 import { ManageMemberSpecVariables } from './manage-members-spec-variables';
-import { BehaviorSubject } from 'rxjs';
-import { HttpParams } from '@angular/common/http';
+import { BehaviorSubject, EMPTY, throwError } from 'rxjs';
+import { HttpErrorResponse, HttpParams } from '@angular/common/http';
 
 describe('ManageMembersService', () => {
   const BASEURL = env.api_root;
@@ -41,13 +41,18 @@ describe('ManageMembersService', () => {
 
   it('should get the organization\'s ID', () => {
     let orgId = '1';
-    spyOn(authService, 'getLocalStorage').and.returnValue(orgId);
+    let spy = spyOn(authService, 'getLocalStorage').and.returnValue(orgId);
     expect(memberService.getOrgId()).toEqual(orgId);
     expect(authService.getLocalStorage).toHaveBeenCalledWith(UserLocalStorage.OrgId);
+    orgId = '';
+    spy.and.returnValue(orgId);
+    expect(memberService.getOrgId()).toEqual(orgId);
   });
 
   it('should get clients', () => {
     let orgId = '1';
+    let errorMessage = 'Forbidden';
+    let statusNumber = 403;
     let orgModeOn = new BehaviorSubject(true);
     let params = new HttpParams();
     params = params.append('organization', String(orgId));
@@ -64,19 +69,32 @@ describe('ManageMembersService', () => {
     memberService.getAllClients().subscribe((data) => {
       expect(data).toEqual(ManageMemberSpecVariables.accessedClients);
     });
+    memberService.getAllClients().subscribe((data) => {
+      expect(data).toEqual(EMPTY);
+    },
+    // If error propogated
+    (error) => {
+      expect(error.status).toEqual(statusNumber);
+      expect(error.statusText).toEqual(errorMessage);
+    });
 
     expect(authService.getOrgMode).toHaveBeenCalledWith();
     const req = httpTestingController.expectOne(`${BASEURL}/user/?` + params.toString());
     expect(req.request.method).toBe("GET");
     req.flush(ManageMemberSpecVariables.employees);
     
-    const req2 = httpTestingController.expectOne(`${BASEURL}/accessClients/`);
-    expect(req2.request.method).toBe("GET");
-    req2.flush(ManageMemberSpecVariables.accessedClients);
+    const req2 = httpTestingController.match(`${BASEURL}/accessClients/`);
+    expect(req2[0].request.method).toBe("GET");
+    req2[0].flush(ManageMemberSpecVariables.accessedClients);
+
+    expect(req2[1].request.method).toBe("GET");
+    req2[1].flush('403 error', { status: statusNumber, statusText: errorMessage });
   });
 
   it('should get paginated clients', () => {
     let orgId = '1';
+    let errorMessage = 'Forbidden';
+    let statusNumber = 403;
     let orgModeOn = new BehaviorSubject(true);
     let params = new HttpParams();
     params = params.append('organization', String(orgId));
@@ -93,14 +111,26 @@ describe('ManageMembersService', () => {
       expect(data).toEqual(ManageMemberSpecVariables.accessedClients);
     });
 
+    memberService.getPaginatedClients(params).subscribe((data) => {
+      expect(data).toEqual(EMPTY);
+    },
+    // If error propogated
+    (error) => {
+      expect(error.status).toEqual(statusNumber);
+      expect(error.statusText).toEqual(errorMessage);
+    });
+
     expect(authService.getOrgMode).toHaveBeenCalledWith();
     const req = httpTestingController.expectOne(`${BASEURL}/user/?` + params.toString() + '&' + params.toString());
     expect(req.request.method).toBe("GET");
     req.flush(ManageMemberSpecVariables.employees);
-    
-    const req2 = httpTestingController.expectOne(`${BASEURL}/accessPagedClients/?` + params.toString());
-    expect(req2.request.method).toBe("GET");
-    req2.flush(ManageMemberSpecVariables.accessedClients);
+
+    const req2 = httpTestingController.match(`${BASEURL}/accessPagedClients/?` + params.toString());
+    expect(req2[0].request.method).toBe("GET");
+    req2[0].flush(ManageMemberSpecVariables.accessedClients);
+
+    expect(req2[1].request.method).toBe("GET");
+    req2[1].flush('403 error', { status: statusNumber, statusText: errorMessage });
   });
 
   it('should update user password', () => {
