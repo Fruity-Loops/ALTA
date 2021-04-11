9bdd6eefd98a9b382915576445d0bab31ba979f9
Neeham <Neehamk@gmail.com>
2021-04-10 21:53:42 -0400

    test: Partially 6.1 working

--- client/e2e/src/epic_6/forgot-password.e2e-spec.ts
+++ client/e2e/src/epic_6/forgot-password.e2e-spec.ts
@@ -1,34 +1,62 @@
 import {ResetPassword} from './forgot-password.po';
 import {Login, Logout} from '../login.po';
-import {Navigation} from '../navigation.po';
-import { GreetingPage } from '../greeting.po';
+import {GreetingPage} from '../greeting.po';
 import {browser, ExpectedConditions} from 'protractor';
+import {SAModifiesInfo} from '../epic_1/sa-modifies-info.po';
 
 /**
  * https://github.com/fruity-loops/alta/issues/198
  */
 describe('AT-6.1: Reset account password', () => {
-  const nav: Navigation = new Navigation();
   const resetPassword: ResetPassword = new ResetPassword();
   const greetingPage: GreetingPage = new GreetingPage();
+  const SAModifies: SAModifiesInfo = new SAModifiesInfo();
+  const loginPage = new Login();
+  const logoutPage = new Logout();
 
-  /**
-   * Login as an Inventory Manager
-   */
-  beforeAll(function init(): void {
-    const loginPage = new Login();
-    loginPage.login_as('im@test.com', 'password', false);
+  it('User Request a New Password', () => {
+    greetingPage.navigateTo();
+    browser.wait(ExpectedConditions.visibilityOf(resetPassword.getForgotCredentialsButton()), 5000);
+    resetPassword.getForgotCredentialsButton().click();
+    browser.wait(ExpectedConditions.visibilityOf(resetPassword.getEmailField()), 5000);
+    resetPassword.getEmailField().sendKeys('alta490test@mailinator.com');
+    resetPassword.getSubmitButton().click();
+    browser.wait(ExpectedConditions.visibilityOf(resetPassword.getCloseButton()), 5000);
+    resetPassword.getCloseButton().click();
   });
 
-  /**
-   * Logout
-   */
-  afterAll(function endit(): void {
-    const logoutPage = new Logout();
+  it('Navigate to email to retrieve the link', () => {
+    browser.waitForAngularEnabled(false);
+    browser.get('https://www.mailinator.com/v4/public/inboxes.jsp?to=alta490test');
+    browser.navigate().refresh();
+    browser.wait(ExpectedConditions.visibilityOf(resetPassword.getLatestEmail()), 5000);
+    resetPassword.getLatestEmail().click();
+    browser.wait(ExpectedConditions.visibilityOf(resetPassword.getTextButton()), 5000);
+    resetPassword.getTextButton().click();
+    // @ts-ignore
+    browser.switchTo().frame('texthtml_msg_body');
+    browser.wait(ExpectedConditions.visibilityOf(resetPassword.getResetLink()), 5000);
+    resetPassword.getResetLink().click();
+    browser.switchTo().defaultContent();
+  });
+
+  it('User provides new password', () => {
+    // browser.wait(ExpectedConditions.urlContains('settings'), 5000);
+    browser.sleep(5000);
+    // browser.wait(ExpectedConditions.visibilityOf(SAModifies.getEditButton()), 5000);
+    browser.refresh();
+    browser.sleep(5000);
+    SAModifies.getEditButton().click();
+    browser.sleep(5000);
+    browser.wait(ExpectedConditions.visibilityOf(SAModifies.getPasswordField()), 5000);
+    SAModifies.getPasswordField().sendKeys('hello');
+    SAModifies.getSaveButton().click();
+    browser.sleep(5000);
     logoutPage.logout();
   });
 
-  it('User Resets their Password', () => {
-    greetingPage.navigateTo();
+  it('User logs in with the new password', () => {
+    loginPage.login_as('alta490test@mailinator.com', 'hello', false);
+    logoutPage.logout();
   });
 });
--- client/e2e/src/epic_6/forgot-password.po.ts
+++ client/e2e/src/epic_6/forgot-password.po.ts
@@ -2,4 +2,31 @@ import {by, element, ElementFinder} from 'protractor';
 
 export class ResetPassword {
 
+  getForgotCredentialsButton(): ElementFinder {
+    return element(by.id('forgot-credentials'));
+  }
+
+  getEmailField(): ElementFinder {
+    return element(by.id('emailAddress'));
+  }
+
+  getSubmitButton(): ElementFinder {
+    return element(by.id('submitButton'));
+  }
+
+  getCloseButton(): ElementFinder {
+    return element(by.id('closeButton'));
+  }
+
+  getLatestEmail(): ElementFinder {
+    return element(by.xpath('(//*[contains(@id,"row_alta490test")]/td[3])[1]'));
+  }
+
+  getTextButton(): ElementFinder {
+    return element(by.xpath('//*[@id="pills-textbuthtml-tab"]'));
+  }
+
+  getResetLink(): ElementFinder {
+    return element(by.xpath('/html/body/a'));
+  }
 }
